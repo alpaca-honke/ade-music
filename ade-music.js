70642a22be414aa64be8b05c77e9918a53ab6910
Alkappa/alpaca-honke <akp@ymail.ne.jp>
2023-09-07 09:57:56 +0900

    Add 1-million-times snooze

--- ade-music.js
+++ ade-music.js
@@ -14,10 +14,9 @@ const videoid = [
 	"sm41410666"/*ハルシネイション・シザーハンズ*/,
 	"sm41720763"/*エターナルアリア*/,
 	"sm41945586"/*シマエナガール*/,
-	"sm42260059"/*クオリア*/
+	"sm42260059"/*クオリア*/,
+    "sm42565288"/*スヌーズ*/
 ];
-const numbers = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
-let shuffled_numbers = [];
 let ind = 0;
 let i = 0;
 let j = 0;
@@ -33,12 +32,12 @@ const video8 = document.getElementById("video8");
 const video9 = document.getElementById("video9");
 const videos = [video0,video1,video2,video3,video4,video5,video6,video7,video8,video9];
 //フィッシャーイェーツのアルゴリズムでシャッフルする関数
-const shuffleArray = (array) => {
-	for (i = array.length -1 ; i > 0 ; i--){
+async function shuffleArray(array) {
+	for (let i = array.length -1 ; i > 0 ; i--){
 		j = Math.floor(Math.random() * (i + 1));
 		[array[i], array[j]] = [array[j], array[i]];
 	}
-};
+}
 
 //ホントはform要素のonsubmitで関数呼び出したかった
 document.getElementById("form").onkeydown = (e)=>{
@@ -47,20 +46,15 @@ document.getElementById("form").onkeydown = (e)=>{
     };
 };
     
-function enter(){
+async function enter(){
     //変数の初期化
     ind = 0;
-	i = 0;
-	shuffled_numbers = [];
 	//ランダムに10個選出
-	shuffleArray(numbers);
-		//test
-		//console.log(numbers);
-	shuffled_numbers = numbers.slice(0,10);
+	await shuffleArray(videoid);
 
     while(ind < 10){
 		//動画を表示
-		videos[ind].setAttribute("src","https://embed.nicovideo.jp/watch/" + videoid[shuffled_numbers[ind]] + "/script?w=640&h=360");
+		videos[ind].setAttribute("src","https://embed.nicovideo.jp/watch/" + videoid[ind] + "/script?w=640&h=360");
 		ind ++;
 	};
 }
